633f0a66cffe43728cf08992596820c3e9ac3e5f
George Spake <gpspake@gmail.com>
2019-06-18 08:29:33 -0500

    add dm favicon
    fixes #16

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,7 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
+    `gatsby-plugin-favicon`,
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
