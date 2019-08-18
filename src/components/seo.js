1aa3412ce950edd6b4d48f4fadf474b97988a7a7
George Spake <gpspake@gmail.com>
2019-08-18 17:22:38 -0500

    seo image

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import devMemphisSeoPic from "../../content/assets/devmemphis-pic.png"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -19,13 +20,14 @@ function SEO({ description, lang, meta, title }) {
             title
             description
             author
+            defaultImage
           }
         }
       }
     `
   )
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
@@ -66,6 +68,10 @@ function SEO({ description, lang, meta, title }) {
         {
           name: `twitter:description`,
           content: metaDescription,
+        },
+        {
+          name: `twitter:image`,
+          content: devMemphisSeoPic,
         },
       ].concat(meta)}
     />
